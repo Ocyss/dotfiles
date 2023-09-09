1e039f80946b186143f61f51b29bf3f204084c3d
Ocyss <qiudie@88.com>
2023-09-09 08:09:09 +0800

    歌词优化

--- eww/scripts/MusicLyrics.py
+++ eww/scripts/MusicLyrics.py
@@ -1,10 +1,10 @@
 #!/bin/env python3
 # coding:utf-8
-'''
+"""
 Author: Ocyss qiudie@88.com
 Date: 2023-07-12 10:50:24
 Import: https://gitee.com/thecasttim/lrc-parser
-'''
+"""
 from mpd import MPDClient
 import time
 import os
@@ -13,7 +13,6 @@ import re
 from enum import Enum
 import signal
 import sys
-
 import mutagen
 
 
@@ -31,39 +30,47 @@ TAG_PATTERN = r"\[.*?\]"
 # 命名分组
 TIME_PATTERN = r"^(?P<p1>\d+:){0,1}(?P<p2>[0-5][0-9]:){0,1}(?P<p3>[0-5][0-9])(?P<p4>\.[\d]{1,3}){0,1}$"
 # 捕获分组
-ENHANCE_TIME_PATTERN_C = r"\<(\d+\:){0,1}([0-5][0-9]\:){0,1}([0-5][0-9])(\.[\d]{1,3}){0,1}\>"
+ENHANCE_TIME_PATTERN_C = (
+    r"\<(\d+\:){0,1}([0-5][0-9]\:){0,1}([0-5][0-9])(\.[\d]{1,3}){0,1}\>"
+)
 # 非捕获分组
-ENHANCE_TIME_PATTERN_N = r"\<(?:\d+\:){0,1}(?:[0-5][0-9]\:){0,1}(?:[0-5][0-9])(?:\.[\d]{1,3}){0,1}\>"
+ENHANCE_TIME_PATTERN_N = (
+    r"\<(?:\d+\:){0,1}(?:[0-5][0-9]\:){0,1}(?:[0-5][0-9])(?:\.[\d]{1,3}){0,1}\>"
+)
 
 # 标签类型
 
 
 class TagType(Enum):
-    ID = 0          # ID标签
-    TIME = 1        # 时间戳标签
-    UNKNOWN = 2      # 未知标签
+    ID = 0  # ID标签
+    TIME = 1  # 时间戳标签
+    UNKNOWN = 2  # 未知标签
 
 
 class LrcParser:
 
-    """ 解析Lrc歌词文件 """
+    """解析Lrc歌词文件"""
 
     def __init__(self, music_file):
         text = ""
-        lrcpath = MusicPath+os.path.splitext(music_file)[0]+".lrc"
+        lrcpath = MusicPath + os.path.splitext(music_file)[0] + ".lrc"
         if os.path.isfile(lrcpath):
-            with codecs.open(lrcpath, 'r', encoding="utf-8") as f:
+            with codecs.open(lrcpath, "r", encoding="utf-8") as f:
                 text = f.read()
         else:
-            tags = mutagen.File(MusicPath+music_file)
+            tags = mutagen.File(MusicPath + music_file)
             for v in tags.keys():
                 if v.startswith("USLT"):
                     text = str(tags.get(v))
                     break
+                elif v == "lyrics":
+                    text = tags.get(v)[0]
+                    break
+
         # 整个歌词文件的内容
         # 检查[]<>等标签括号是否匹配
         if not self.is_tags_valid(text):
-            raise SyntaxError('lrc文件标签[]或<>不匹配')
+            raise SyntaxError("lrc文件标签[]或<>不匹配")
 
         # 歌词列表
         #   每个元素为字典{
@@ -80,22 +87,21 @@ class LrcParser:
         tags = [tag[1:-1] for tag in re.findall(TAG_PATTERN, text)]
 
         # 提取所有标签标注的内容
-        segments = [segment.strip()
-                    for segment in re.split(TAG_PATTERN, text)]
-        segments = segments[1:]          # split结果包含第一个标签前的字符(包括空字符), 将其丢弃
+        segments = [segment.strip() for segment in re.split(TAG_PATTERN, text)]
+        segments = segments[1:]  # split结果包含第一个标签前的字符(包括空字符), 将其丢弃
 
         tags_num = len(tags)
         if tags_num != len(segments):
-            raise ValueError('标签数与标注内容个数不匹配, 请检查lrc文件')
+            raise ValueError("标签数与标注内容个数不匹配, 请检查lrc文件")
 
         # 逐段解析标签及其内容
         for i in range(0, tags_num):
             tag_type = self.get_tag_type(tags[i])
             # 判断标签是时间标签还是ID标签, 分别处理
             if tag_type == TagType.TIME:
-                res = {'time': self.parse_time_tag(tags[i])}
+                res = {"time": self.parse_time_tag(tags[i])}
                 # 普通格式
-                res['lyric'] = segments[i]
+                res["lyric"] = segments[i]
                 self.lyrics.append(res)
 
     @staticmethod
@@ -109,13 +115,13 @@ class LrcParser:
             return True
 
         res = list()
-        pair = {']': '[', '>': '<'}
+        pair = {"]": "[", ">": "<"}
         for c in text:
-            if c == ']' or c == '>':
+            if c == "]" or c == ">":
                 if not res or res[-1] != pair[c]:
                     return False
                 res.pop()
-            elif c == '[' or c == '<':
+            elif c == "[" or c == "<":
                 res.append(c)
 
         return len(res) == 0
@@ -153,21 +159,21 @@ class LrcParser:
 
         # 毫秒
         ms = 0
-        if time_parts['p4'] is not None:
+        if time_parts["p4"] is not None:
             # ".xxx秒" ---> 毫秒
-            ms = float(time_parts['p4'])
+            ms = float(time_parts["p4"])
 
         # 秒
         s = 0
-        if time_parts['p3'] is not None:
-            s = int(time_parts['p3'])
+        if time_parts["p3"] is not None:
+            s = int(time_parts["p3"])
 
         # 小时与分钟
-        if time_parts['p2'] is None:
+        if time_parts["p2"] is None:
             h = 0
-            if time_parts['p1'] is not None:
+            if time_parts["p1"] is not None:
                 # <p1>xx:<p3>xx(<p4>.xx)形式( xx:xx(.xx) )
-                minute = int(time_parts['p1'][:-1])
+                minute = int(time_parts["p1"][:-1])
                 if minute >= 60:
                     h = minute // 60
                     minute = minute % 60
@@ -175,47 +181,49 @@ class LrcParser:
                 # <p3>xx(<p4>.xx)形式( xx(.xx) )
                 minute = 0
         else:
-            h = int(time_parts['p1'][:-1])
-            minute = int(time_parts['p2'][:-1])
+            h = int(time_parts["p1"][:-1])
+            minute = int(time_parts["p2"][:-1])
 
-        return h*60*60+minute*60+s+ms
+        return h * 60 * 60 + minute * 60 + s + ms
 
     def get_lyrics(self):
         # 获取歌词列表
         return self.lyrics
+
 
 # def len_format(string,width=23,fill= ' ',ed = ''):#格式输出函数,默认格式填充用单空格,不换行。
 #     count = wcwidth.wcswidth(string)-len(string) #宽字符数量
 #     width = width - count if width >= count else 0
 #     return '{0:{1}^{2}}'.format(string,fill,width)
 
+client = MPDClient()
+
 
 def getLyrics():
-    client = MPDClient()
     client.connect("localhost", 6600)
     cur = None
-    file = None
     lrc = None
     l = None
     while True:
         try:
-            if cur is None or cur['file'] != client.currentsong()['file']:
+            if cur is None or cur["file"] != client.currentsong()["file"]:
                 cur = client.currentsong()
-                lrc = LrcParser(cur['file']).get_lyrics()
+                lrc = LrcParser(cur["file"]).get_lyrics()
                 l = 0
             status = client.status()
-            t = float(status['elapsed'])
-            while l < len(lrc) and lrc[l]['time'] < t:
+            t = float(status["elapsed"])
+            while l < len(lrc) and lrc[l]["time"] < t:
                 l += 1
-            print("[{0}]".format(lrc[l-1]['lyric']))
+            print("[{0}]".format(lrc[l - 1]["lyric"]))
             sys.stdout.flush()
-        except BaseException as e:
+            time.sleep(0.4)
+        except BaseException:
             break
-        time.sleep(0.4)
     getLyrics()
 
 
 def quit(args, a):
+    client.remove_command()
     exit(0)
 
 
